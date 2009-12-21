c8081b2a0f1bf0f1a95a1d11d9a424108f9a3a42
Jeena Paradies <spam@jeenaparadies.net>
2009-12-21 22:35:49 +0100

    changed description

--- mk_pasteboard_tinyurl.sh
+++ mk_pasteboard_tinyurl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# tinyurl.sh takes a url from the message buffer and
+# mk_pasteboard_tinyurl.sh takes a url from the message buffer and
 # turns it to a TinyURL which its writes back to it
 
 curl -s http://tinyurl.com/api-create.php?url=`pbpaste` | pbcopy
