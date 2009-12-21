d83adfeea53e21b62d5d764bd124aa9915b56790
Jeena Paradies <spam@jeenaparadies.net>
2009-12-21 22:41:05 +0100

    added some documentation

--- chalmers_login.sh
+++ chalmers_login.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# chalmers_login.sh logges you in into the Chalmers NOMAD wireless
+# network. You have to chmod 755 it and I recommend to start it
+# via Quicksilver. It uses growl to notify you about success or
+# faulire.
+#
+# Author: Jeena Paradies <spam@jeenaparadies.net>
+# This script is public domain.
+# This script is OS X only.
+
 USERNAME="uname"
 PASSWD="password"
 
